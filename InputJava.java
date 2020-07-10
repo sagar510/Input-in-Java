6b4f6c6f483d5d3110ba55bfba0ad9cb4e2e9745
Sagar Pal <57063071+sagar510@users.noreply.github.com>
2020-07-10 23:10:04 +0530

    Add files via upload

---
+++ InputJava.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class InputJava {
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name=obj.nextLine();
+        System.out.println("Your Name is "+name);
+    }
+}
